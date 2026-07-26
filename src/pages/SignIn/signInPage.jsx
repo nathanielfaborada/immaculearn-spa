192fa96327f50e74a7cf91573dbf84eb4fc8f79b
nathanielfaborada <faboradanathaniel@gmail.com>
2026-07-26 12:34:15 +0800

    Launch Spa

--- src/pages/SignIn/signInPage.jsx
+++ src/pages/SignIn/signInPage.jsx
@@ -112,7 +112,7 @@ const LoginPage = () => {
   const handleGmailLogin = async () => {
     const baseUrl =
       config.VITE_ENV === "production"
-        ? `https://immaculearn.up.railway.app/v1/account/oauth/google/redirect`
+        ? `${config.API_URL}/account/oauth/google/redirect`
         : "http://localhost:3000/v1/account/oauth/google/redirect";
 
     const popup = window.open(
